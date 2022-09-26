37c275a1221ea07d314083352f6737c561f5e563
Joe <jwschurer@gmail.com>
2022-09-26 11:57:56 -0400

    sd

--- test_sketch/test_sketch.ino
+++ test_sketch/test_sketch.ino
@@ -1,10 +1,23 @@
+/*
+  SD card read/write
+  This example shows how to read and write data to and from an SD card file
+  The circuit:
+   SD card attached to SPI bus as follows:
+ ** MOSI - pin 11
+ ** MISO - pin 12
+ ** CLK - pin 13
+ ** CS - pin 4 (for MKRZero SD: SDCARD_SS_PIN)
+*/
 
 #include <Wire.h>
 #include <LIDARLite.h>
+#include <SPI.h>
+#include <SD.h>
 
-// Globals
+File myFile;
 LIDARLite lidarLite;
 int cal_cnt = 0;
+int iter =0;
 
 void setup()
 {
@@ -12,6 +25,46 @@ void setup()
 
   lidarLite.begin(0, true); // Set configuration to default and I2C to 400 kHz
   lidarLite.configure(0); // Change this number to try out alternate configurations
+
+   Serial.print("Initializing SD card...");
+
+  if (!SD.begin(4)) {
+    Serial.println("initialization failed!");
+    while (1);
+  }
+  Serial.println("initialization done.");
+
+  myFile = SD.open("test.txt", FILE_WRITE);
+
+  // if the file opened okay, write to it:
+  if (myFile) {
+    Serial.print("Writing to test.txt...");
+    myFile.println("testing 1, 2, 3.");
+    // close the file:
+    myFile.close();
+    Serial.println("done.");
+  } else {
+    // if the file didn't open, print an error:
+    Serial.println("error opening test.txt");
+  }
+
+  // re-open the file for reading:
+  myFile = SD.open("test.txt");
+  if (myFile) {
+    Serial.println("test.txt:");
+
+    // read from the file until there's nothing else in it:
+    while (myFile.available()) {
+      Serial.write(myFile.read());
+    }
+    // close the file:
+    myFile.close();
+  } else {
+    // if the file didn't open, print an error:
+    Serial.println("error opening test.txt");
+  }
+
+  myFile = SD.open("test.txt", FILE_WRITE);
 }
 
 void loop()
@@ -35,4 +88,12 @@ void loop()
   Serial.println(" cm");
 
   delay(10);
+
+  
+  if(iter < 10){
+    myFile.println(dist);
+    iter++;
+  } else if(iter == 10){
+    myFile.close();
+    }
 }
