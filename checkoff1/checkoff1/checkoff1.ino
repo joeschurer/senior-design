7385a3352678753d823c62d94f2c28c03dac6654
Joe <jwschurer@gmail.com>
2022-10-09 17:56:10 -0400

    sdcard times

--- checkoff1/checkoff1/checkoff1.ino
+++ checkoff1/checkoff1/checkoff1.ino
@@ -4,13 +4,18 @@
 #include <SPI.h>
 #include <SD.h>
 
-
-
+String dataBuf;
 LIDARLite_v3HP myLidarLite;
 
 #define FAST_I2C
 File myFile;
 int config_val = 7;
+
+const uint16_t bufSize = 240;
+uint8_t buf[bufSize];
+uint16_t index = 0;
+
+unsigned int chunkSize = 512;
 
 enum rangeType_T
 {
@@ -20,8 +25,9 @@ enum rangeType_T
     RANGE_TIMER
 };
 
-void setup()
-{
+void setup(){
+    dataBuf.reserve(1024);
+    chunkSize = myFile.availableForWrite();
     pinMode(7, OUTPUT);
     // Initialize Arduino serial port (for display of ASCII output to PC)
     Serial.begin(9600);
@@ -47,27 +53,44 @@ void setup()
     SD.remove("test.txt");
     myFile = SD.open("test.txt", FILE_WRITE);
 
-    
+    //Discard first distance
+     
 }
 
 
 void loop(){
-  myFile.println("-------------New Run-------------");
-    delay(1000);
+    delay(10000);
     uint16_t distance;
     uint8_t  newDistance = 0;
     uint8_t  c;
 
     int startTime = millis();
-    for(int i=0;i<100;i++){
+    for(int i=0;i<1000;i++){
       //use distanceFast
       newDistance = distanceFast(&distance);
-      myFile.println(distance);
+      myFile.print(distance);
+      //bufWrite(distance);
       Serial.println(distance);
+
+      /*
+      if(index>=bufSize){
+        myFile.write(buf,index);
+        index = 0;
+      }
+      buf[index] = highByte(distance);
+      index++;
+      buf[index] = lowByte(distance);
+      index++;
+      */
+      
     }
 
+    //bufFlush();
     int endTime = millis();
+    //bufFlush();
     Serial.println("Time: ");
+    myFile.print("\n");
+    myFile.print("Time: ");
     int finalTime = endTime-startTime;
     Serial.println(finalTime);
     myFile.println(finalTime);
@@ -103,4 +126,21 @@ uint8_t distanceSingle(uint16_t * distance)
     *distance = myLidarLite.readDistance();
 
     return 1;
+}
+
+
+//For string buffer
+void bufWrite(uint16_t data){
+  dataBuf+= data;
+  if (chunkSize && dataBuf.length() >= chunkSize) {
+    myFile.write(dataBuf.c_str(), chunkSize);
+    dataBuf.remove(0, chunkSize);
+  }
+}
+
+void bufFlush(){
+    if (dataBuf.length() != 0)
+    {
+    myFile.write(dataBuf.c_str(), dataBuf.length());
+    }
 }
